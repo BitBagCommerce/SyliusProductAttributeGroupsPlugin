825f6b26c09a9e2e6a9053ab1b4eb72150a9513d
TheSylwio <sylwester.gorol1@gmail.com>
2022-01-03 13:03:45 +0100

    Handle submit form event

--- src/Form/Type/GroupType.php
+++ src/Form/Type/GroupType.php
@@ -11,24 +11,65 @@ declare(strict_types=1);
 namespace BitBag\SyliusProductAttributeGroupsPlugin\Form\Type;
 
 use BitBag\SyliusProductAttributeGroupsPlugin\Entity\Group;
+use BitBag\SyliusProductAttributeGroupsPlugin\Factory\AttributeFactory;
+use BitBag\SyliusProductAttributeGroupsPlugin\Factory\GroupFactory;
+use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Sylius\Component\Product\Model\ProductAttribute;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
-class GroupType extends AbstractResourceType {
-	public function __construct() {
-		parent::__construct(Group::class);
-	}
+class GroupType extends AbstractResourceType
+{
+    private GroupFactory $groupFactory;
 
-	public function buildForm(FormBuilderInterface $builder, array $options): void {
-		$builder
-			->add('name', TextType::class)
-			->add('attributes', EntityType::class, [
-				'class' => ProductAttribute::class,
-				'multiple' => true,
-				'mapped' => false
-			]);
-	}
+    private AttributeFactory $attributeFactory;
+
+    private EntityManagerInterface $em;
+
+    public function __construct(
+        GroupFactory $groupFactory,
+        AttributeFactory $attributeFactory,
+        EntityManagerInterface $em
+    )
+    {
+        parent::__construct(Group::class);
+        $this->groupFactory = $groupFactory;
+        $this->attributeFactory = $attributeFactory;
+        $this->em = $em;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('attributes', EntityType::class, [
+                'class' => ProductAttribute::class,
+                'multiple' => true,
+                'mapped' => false,
+            ]);
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event): void {
+            $form = $event->getForm();
+            $name = $form->get('name')->getData();
+            $attributes = $form->get('attributes')->getData();
+
+            $group = $this->groupFactory->createNew();
+            $group->setName($name);
+
+            foreach ($attributes as $attribute) {
+                $newAttribute = $this->attributeFactory->createNew();
+                $newAttribute->setSyliusAttribute($attribute);
+                $newAttribute->setGroup($group);
+
+                $group->addAttribute($newAttribute);
+            }
+
+            $this->em->persist($group);
+            $this->em->flush();
+        });
+    }
 }
