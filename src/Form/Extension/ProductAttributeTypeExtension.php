591d44415445ab48d1aa79290f2c0d012ef45739
TheSylwio <sylwester.gorol1@gmail.com>
2022-01-10 18:41:16 +0100

    Handle attribute group field logic

--- src/Form/Extension/ProductAttributeTypeExtension.php
+++ src/Form/Extension/ProductAttributeTypeExtension.php
@@ -4,14 +4,31 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusProductAttributeGroupsPlugin\Form\Extension;
 
+use BitBag\SyliusProductAttributeGroupsPlugin\Entity\Attribute;
 use BitBag\SyliusProductAttributeGroupsPlugin\Entity\Group;
+use BitBag\SyliusProductAttributeGroupsPlugin\Form\DataTransformer\GroupTransformer;
+use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductAttributeType;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 final class ProductAttributeTypeExtension extends AbstractTypeExtension
 {
+	private RepositoryInterface $repository;
+	private EntityManagerInterface $entityManager;
+	private FactoryInterface $factory;
+
+	public function __construct(RepositoryInterface $repository, EntityManagerInterface $entityManager, FactoryInterface $factory){
+		$this->repository = $repository;
+		$this->entityManager = $entityManager;
+		$this->factory = $factory;
+	}
+
 	public function buildForm(FormBuilderInterface $builder, array $options): void
     {
 		$builder->add('group', EntityType::class, [
@@ -20,8 +37,43 @@ final class ProductAttributeTypeExtension extends AbstractTypeExtension
 			'mapped' => false,
 			'required' => false,
 			'placeholder' => '',
-			'empty_data'  => null,
         ]);
+
+		$builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event){
+			$form = $event->getForm();
+			/** @var Attribute $attribute */
+			$attribute = $this->repository->findOneBy(['syliusAttribute' => $event->getData()]);
+
+			if (!$attribute) {
+				return;
+			}
+
+			$group = $form->get('group');
+			$group->setData($attribute->getGroup());
+		});
+
+		$builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+			$form = $event->getForm();
+			$group = $form->get('group')->getData();
+
+			if (!$form->isValid()) {
+				return;
+			}
+
+			$previousGroup = $this->repository->findOneBy(['syliusAttribute' => $event->getData()])->getGroup();
+
+			if ($previousGroup !== $group) {
+				$this->entityManager->remove($previousGroup);
+			}
+
+			/** @var Attribute $attribute */
+			$attribute = $this->factory->createNew();
+			$attribute->setGroup($group);
+			$attribute->setSyliusAttribute($event->getData());
+
+			$this->entityManager->persist($attribute);
+			$this->entityManager->flush();
+		});
     }
 
     public static function getExtendedTypes(): iterable
